20d71a5713f2d27eb6f1befdc937bc75c7409390
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2025-08-02 12:08:28 +0300

    Fix queue hang on request abort

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { FastifyPluginAsync } from "fastify";
+import type { FastifyPluginAsync, FastifyRequest } from "fastify";
 import fp from "fastify-plugin";
 import { randomUUID } from "node:crypto";
 import EventEmitter from "node:events";
@@ -13,6 +13,8 @@ const plugin: FastifyPluginAsync = async (app) => {
 	const queue: string[] = [];
 	const event = new EventEmitter<{ "onShift": [id: string] }>();
 
+	event.setMaxListeners(0);
+
 	app.addHook("onRequest", (req, reply, done) => {
 		req.queueId = randomUUID();
 
@@ -26,18 +28,24 @@ const plugin: FastifyPluginAsync = async (app) => {
 				}
 			};
 
-			event.on("onShift", callback);
+			event.addListener("onShift", callback);
 		}
 		else
 			done();
 	});
 
-	app.addHook("onResponse", async (req) => {
-		if (req.queueId) {
-			queue.splice(queue.indexOf(req.queueId), 1);
+	const shift = (req: FastifyRequest, mustShift?: boolean) => {
+		const { queueId, raw: { aborted } } = req;
+		if (queueId && (aborted || mustShift)) {
+			queue.shift();
 			event.emit("onShift", queue[0]);
 		}
-	});
+	};
+
+	app.addHook("preParsing", async (req) => shift(req));
+	app.addHook("preValidation", async (req) => shift(req));
+	app.addHook("preHandler", async (req) => shift(req));
+	app.addHook("onSend", async (req) => shift(req, true));
 };
 
 export default fp(plugin);
